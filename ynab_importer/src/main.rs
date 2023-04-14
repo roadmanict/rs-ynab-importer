d6ac2ee1a71c992020aa76e2fa3931099d7e1672
Geert Weggemans <geert@pop-os.localdomain>
2023-04-14 10:27:48 +0200

    Remove root src

--- ynab_importer/src/main.rs
+++ ynab_importer/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let paths = fs::read_dir("/tmp/asn-bank").unwrap();
+
+    for path in paths {
+        println!("path {}", path.unwrap().path().display());
+    }
 }
