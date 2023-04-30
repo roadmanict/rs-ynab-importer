9b2a31a2b5f368405630a87a6566354bb69a8dba
Geert Weggemans <geert@pop-os.localdomain>
2023-04-30 21:53:20 +0200

    Toggle on/off new ntry

--- ynab_importer/src/main.rs
+++ ynab_importer/src/main.rs
@@ -2,28 +2,41 @@ extern crate xml;
 
 use std::{fs::File, io::BufReader};
 
-use xml::{reader::XmlEvent, EventReader};
+use xml::{attribute::OwnedAttribute, reader::XmlEvent, EventReader};
 
 fn main() {
     let file = File::open("resources/example.xml").unwrap();
     let file = BufReader::new(file);
 
     let parser = EventReader::new(file);
+    let mut in_entry = false;
 
     for e in parser {
         match e {
             Ok(XmlEvent::StartElement {
-                name,
-                attributes,
-                namespace,
-            }) => {
-                match name.local_name.to_lowercase().as_str() {
-                    "ntry" => println!("{:?}", attributes),
-                    _ => {}
+                name, attributes, ..
+            }) => match name.local_name.to_lowercase().as_str() {
+                "ntry" => in_entry = true,
+                st => {
+                    println!("{}, {}, {:?}", st, in_entry, attributes);
                 }
-            }
-            Ok(XmlEvent::EndElement { name }) => println!("{}", name),
+            },
+            Ok(XmlEvent::EndElement { name }) => match name.local_name.to_lowercase().as_str() {
+                "ntry" => in_entry = false,
+                _ => {}
+            },
             Err(_) => todo!(),
+            _ => {}
+        }
+    }
+}
+
+fn parse_entry(attributes: Vec<OwnedAttribute>) {
+    println!("entry {:?}", attributes);
+    for attribute in attributes.iter() {
+        println!("{}", attribute);
+        match attribute.name.local_name.to_lowercase().as_str() {
+            "amt" => println!("{}", attribute.value),
             _ => {}
         }
     }
