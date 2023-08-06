21fb60e3d1b40649d58ed8e8041decfdbd7f6003
Geert Weggemans <geert@pop-os.localdomain>
2023-08-06 17:18:49 +0200

    Add integration test

--- camt053_parser/tests/parse_camt_053_test.rs
+++ camt053_parser/tests/parse_camt_053_test.rs
@@ -8,7 +8,19 @@ fn name() {
     let camt053_parser = Camt053Parser::create();
     let file_contents = fs::read_to_string("resources/example.xml").expect("File to be read");
 
-    let mut result = camt053_parser.parse_file(&file_contents).expect("xml file to be parsed");
+    let mut result = camt053_parser
+        .parse_file(&file_contents)
+        .expect("xml file to be parsed");
 
-    assert_eq!(result.remove(0), Entry::new("".to_string(), "".to_string(), None, None, None, None));
+    assert_eq!(
+        result.remove(0),
+        Entry::new(
+            "NL00BANK0000000000".to_string(),
+            "2023-04-28".to_string(),
+            None,
+            None,
+            None,
+            None
+        )
+    );
 }
