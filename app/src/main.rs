90f110fbf68aabba4038fe4a9414c12308eaed41
Geert Weggemans <geert@Geerts-MacBook-Pro.local>
2023-08-30 11:12:33 +0200

    Add option to debug empty payees

--- app/src/main.rs
+++ app/src/main.rs
@@ -8,6 +8,8 @@ use ynab_csv::YnabCsvSerializer;
 #[command(author, version, about)]
 struct Args {
     file: String,
+    #[arg(short, long, default_value_t = false)]
+    show_empty_payee: bool,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -16,7 +18,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let camt053_parser = Camt053Parser::create();
     let ynab_csv_serializer = YnabCsvSerializer::create();
     let xml = fs::read_to_string(&args.file)?;
-    let entries = camt053_parser.parse_file(&xml)?;
+    let mut entries = camt053_parser.parse_file(&xml)?;
+
+    if args.show_empty_payee {
+        entries = entries
+            .into_iter()
+            .filter(|e| e.payee.is_none())
+            .collect::<Vec<_>>();
+    }
+
     let ynab_csv = ynab_csv_serializer.serialize(entries)?;
 
     println!("{}", ynab_csv);
